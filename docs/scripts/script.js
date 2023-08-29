dcc4c2b6235d9e0649ab21a3c507d9d1c8f0aada
TheeVoid <92278175+TheeVoid@users.noreply.github.com>
2023-08-29 19:53:20 +0200

    another no name commit

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,8 +1,31 @@
-
-
 function loadHTML(){
+
+    return fetch("../data/navigation.json", {
+        method: 'GET',
+        headers:{
+            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
+        },
+
+    })
+    .then(console.log(Response.json()))    
+
+
+
+
+}
+/*
+        .then(data => console.log(data))
+        .then(response => document.getElementById('content-load').innerHTML = response)
+        .catch(error => console.log(error));
+*/
+
+/*
     fetch('../browse/')
     .then(response=> response.text())
     .then(text=> document.getElementById('content-load').innerHTML = text);
-  }
+*/
+
+
+
 
