bd741ad91590b852a4fe87e7cd8926075bf7f12c
TheeVoid <92278175+TheeVoid@users.noreply.github.com>
2023-09-09 18:09:25 +0200

    another commit

---
+++ docs/scripts/core/clickItem.js
@@ -0,0 +1,7 @@
+
+async function eventListen(){
+
+document.getElementById("list-link-id").addEventListener('onclick',  console.log("Hello World!"));
+
+
+}
--- docs/scripts/misc/mainBackground/sketch.js
+++ docs/scripts/misc/mainBackground/sketch.js
@@ -1,80 +1,41 @@
-const particles = [];
+let particles = [];
+const num = 1000;
+
+const noiseScale = 0.01/2;
+
 function setup() {
-	createCanvas(window.innerWidth, window.innerHeight);
-	
-	const particlesLength = Math.min(Math.floor(window.innerWidth / 10), 100);
-	for(let i=0; i<particlesLength; i++) {
-		particles.push(new Particle());
-	}
+  createCanvas(windowWidth, windowHeight);
+  for(let i = 0; i < num; i ++) {
+    particles.push(createVector(random(width), random(height)));
+  }
+  
+  stroke(255);
+  // For a cool effect try uncommenting this line
+  // And comment out the background() line in draw
+  // stroke(255, 50);
+  clear();
 }
 
 function draw() {
-	background(20);
-	
-	particles.forEach((particle, idx) => {
-		particle.update();
-		particle.draw();
-		particle.checkParticles(particles.slice(idx));
-	});
+  background(0, 10);
+  for(let i = 0; i < num; i ++) {
+    let p = particles[i];
+    point(p.x, p.y);
+    let n = noise(p.x * noiseScale, p.y * noiseScale, frameCount * noiseScale * noiseScale);
+    let a = TAU * n;
+    p.x += cos(a);
+    p.y += sin(a);
+    if(!onScreen(p)) {
+      p.x = random(width);
+      p.y = random(height);
+    }
+  }
 }
 
-class Particle {
-	constructor() {
-		this.pos = createVector(random(width), random(height));
-		this.vel = createVector(random(-2, 2), random(-2, 2));
-		this.size = 5;
-	}
-	
-	update() {
-		this.pos.add(this.vel);
-		this.edges();
-	}
-	
-	draw() {
-		noStroke();
-		fill('rgba(255, 255, 255, 0.5)');
-		circle(this.pos.x, this.pos.y, this.size * 2);
-	}
-	
-	edges() {
-		if(this.pos.x < 0 || this.pos.x > width) {
-			this.vel.x *= -1;
-		}
-		
-		if(this.pos.y < 0 || this.pos.y > height) {
-			this.vel.y *= -1;
-		}
-		
-// 		if(this.pos.x > width) {
-// 			this.pos.x = 0;
-// 		}
-		
-// 		if(this.pos.y > height) {
-// 			this.pos.y = 0;
-// 		}
-	}
-	
-	checkParticles(particles) {
-		particles.forEach(particle => {
-			const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
-			if(d < 120) {
-				const alpha = map(d, 0, 120, 0, 0.25)
-				stroke(`rgba(255, 255, 255, ${alpha})`);
-				line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y)
-			}
-		});
-	}
+function mouseReleased() {
+  noiseSeed(millis());
 }
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
+function onScreen(v) {
+  return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
+}
